ed59ca21f400cb1b39c87e355f07ed598e49c953
Travis CI User <travis@example.org>
2019-10-18 10:04:14 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,5 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["demo_utils"] = {"name":"","files":["lib.rs","logging.rs"]};
+sourcesIndex["rubble"] = {"name":"","dirs":[{"name":"att","files":["handle.rs","mod.rs","pdus.rs","server.rs","uuid.rs"]},{"name":"gatt","files":["characteristic.rs","mod.rs"]},{"name":"l2cap","files":["mod.rs","signaling.rs"]},{"name":"link","files":["ad_structure.rs","advertising.rs","channel_map.rs","comp_id.rs","connection.rs","data.rs","device_address.rs","features.rs","filter.rs","llcp.rs","mod.rs","queue.rs","responder.rs","seq_num.rs"]}],"files":["beacon.rs","bytes.rs","config.rs","crc.rs","error.rs","lib.rs","log.rs","phy.rs","security.rs","time.rs","utils.rs","uuid.rs"]};
+sourcesIndex["rubble_nrf52"] = {"name":"","files":["lib.rs","radio.rs","timer.rs"]};
+createSourceSidebar();
